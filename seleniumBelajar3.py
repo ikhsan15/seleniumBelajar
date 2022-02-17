29015a9e0fbe5ecde5a5021bbcdc5d4bb23f4544
ikhsan15 <ikhsan15nur@gmail.com>
2022-02-17 16:44:52 +0700

    handling alerts

---
+++ seleniumBelajar3.py
@@ -0,0 +1,23 @@
+from lib2to3.pgen2 import driver
+from webbrowser import get
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://demoqa.com/alerts")
+time.sleep(5)
+
+#  ===================================================================================================
+# CASE 1 =============================================================================================
+# driver.find_element_by_id("alertButton").click()
+# time.sleep(5)
+# handle alert with accept
+# driver.switch_to.alert.accept()
+
+#  ===================================================================================================
+# CASE 2 =============================================================================================
+driver.find_element_by_id("confirmButton").click()
+time.sleep(5)
+# handle alert with cancel
+driver.switch_to.alert.dismiss()
