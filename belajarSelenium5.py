6bea39f7ec41e17acbafd2966122bbfd9384f86f
ikhsan15 <ikhsan15nur@gmail.com>
2022-03-02 10:24:09 +0700

    update implicitly wait

--- belajarSelenium5.py
+++ belajarSelenium5.py
@@ -9,8 +9,10 @@ import time
 driver = webdriver.Chrome()
 
 driver.implicitly_wait(10)
-driver.get("https://demoqa.com/login")
-time.sleep(2)
-driver.get("https://demoqa.com/books")
-time.sleep(2)
-driver.find_element_by_id("login").click()
+# hanya sekali declare dalam 1 project / file
+# misalnya explicitly wait nya 10 detik
+# setiap elemen yg di cek akan di tunggu max 10 detik
+
+driver.get("https://demoqa.com/login")  # di tunggu max 10 detik
+driver.get("https://demoqa.com/books")  # di tunggu max 10 detik
+driver.find_element_by_id("login").click()  # di tunggu max 10 detik
